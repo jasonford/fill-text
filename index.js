dce16e2066e88d017567fb18d1dd961ffd218f7c
jasonford <fordjaso.n@gmail.com>
2016-07-18 13:17:13 -0700

    isolated jquery dependency to width and height functions

--- index.js
+++ index.js
@@ -1,54 +1,81 @@
 function fillText(element, alignment) {
+    function style(el, styles) {
+        for (var field in styles) {
+            el.style[field] = styles[field];
+        }
+    }
 
-    var text = $(element).contents();
+    function width(el, width) {
+        return width? $(el).width(width) : $(el).width();
+        if (width !== undefined) {
+          el.style.width = width + 'px';
+        }
+        return parseFloat(window.getComputedStyle(el, null).getPropertyValue('width'));
+    }
+
+    function height(el, height) {
+        return height? $(el).height(height) : $(el).height();
+        if (height !== undefined) {
+          el.style.height = height + 'px';
+        }
+        return parseFloat(window.getComputedStyle(el, null).getPropertyValue('height'));
+    }
+
+    var children = element.childNodes;
 
     var sizer;
 
     function fit() {
 
-        if (text.text().trim() == '') return;
-        if (!$.contains(document, element)) return;
-        text.detach();
-        var text_element = $('<div>');
-        text_element.css({
+        //  edge cases that would cause infinite loops below
+        if (element.textContent.trim() == '') return;
+        if (!document.contains(element)) return;
+
+        var text_element = document.createElement('div');
+        style(text_element, {
             position : 'relative',
             display : 'inline-block'
         });
         
-        if (sizer) sizer.remove();
-        text_element.append(text);
-        sizer = $('<div class="sizer">');
-        sizer.append(text_element);
-        sizer[0].style.fontSize = '12px';
-        $(element).append(sizer);
-        var text_aspect_ratio = $(text_element).width() / $(text_element).height();
-        var aspect_ratio = $(element).width() / $(element).height();
+        //  remove sizer if already exists
+        if (sizer) element.removeChild(sizer);
+        children.forEach(function (child) {
+          text_element.appendChild(child);
+        });
+        sizer = document.createElement('div');
+        sizer.appendChild(text_element);
+
+        sizer.style.fontSize = '12px';
+
+        element.appendChild(sizer);
+
+        var text_aspect_ratio = width(text_element) / height(text_element);
+        var aspect_ratio = width(element) / height(element);
         if (text_aspect_ratio > aspect_ratio) {
-            $(sizer).width($(text_element).width());
-            $(sizer).height($(text_element).width() / aspect_ratio);
+            width(sizer, width(text_element));
+            height(sizer, width(text_element) / aspect_ratio);
         }
         else {
-            $(sizer).height($(text_element).height());
-            $(sizer).width($(text_element).height() * aspect_ratio);
+            height(sizer, height(text_element));
+            width(sizer, height(text_element) * aspect_ratio);
         }
-        var w = sizer.width();
-        var h = sizer.height();
+        var w = width(sizer);
+        var h = height(sizer);
         var scaleFactor = 0.9;
         while (true) {
             w *= scaleFactor;
             h *= scaleFactor;
-            sizer.width(w);
-            sizer.height(h);
-            if (sizer.width() < text_element.width() || sizer.height() < text_element.height()) {
-                sizer.width(w/scaleFactor);
-                sizer.height(h/scaleFactor);
+            width(sizer, w);
+            height(sizer, h);
+            if (width(sizer) < width(text_element)|| height(sizer) < height(text_element)) {
+                width(sizer, w/scaleFactor);
+                height(sizer, h/scaleFactor);
                 break;
             }
         }
         //    the sizer is now closest aspect ratio as its parent's
-        var scale = $(element).width() / sizer.width();
-        text_aspect_ratio = text_element.width() / text_element.height();
-        sizer.css({
+        var scale = width(element) / width(sizer);
+        style(sizer, {
             'webkitTransformOrigin': '0% 0%',
             'mozTransformOrigin': '0% 0%',
             'msTransformOrigin': '0% 0%',
@@ -57,8 +84,8 @@ function fillText(element, alignment) {
             'transform' : 'scale(' + scale + ', ' + scale + ')'
         });
 
-        var top = (sizer.height() - text_element.height())/2/scale;
-        var left = (sizer.width() - text_element.width())/2/scale;
+        var top = (height(sizer) - height(text_element))/2/scale;
+        var left = (width(sizer) - width(text_element))/2/scale;
         var right = 'auto';
         var bottom = 'auto';
         
@@ -78,17 +105,17 @@ function fillText(element, alignment) {
           top = 'auto';
         }
 
-        text_element.css({
+        style(text_element, {
             position: 'absolute',
             top : top,
             left : left,
             right : right,
             bottom : bottom
         });
-        $(element).css({opacity : 1});
+        style(element, {opacity : 1});
     }
     var mostRecentFrame = requestAnimationFrame(fit);
-    $(element).on('elementresize', function () {
+    element.addEventListener('elementresize', function () {
       var frame = requestAnimationFrame(function () {
         if (frame === mostRecentFrame) fit();
       });
--- test/test.js
+++ test/test.js
@@ -9,7 +9,18 @@
 //    place div in DOM
 //    ensure text is properly sized
 
+var passed = 0;
+var failed = 0;
+
 var tests = [{
+    "text": "mls h",
+    "dimensions": [
+        95,
+        828
+    ],
+    "font": "Arial",
+    "alignment": "top"
+}, {
     "text": "b",
     "dimensions": [
         496,
@@ -26,6 +37,17 @@ var tests = [{
     "font": "Arial",
     "alignment": "top"
 }];
+
+//  run test with no text content to ensure it does not fail
+run({
+    "text": "", //  empty test
+    "dimensions": [
+        496,
+        780
+    ],
+    "font": "sans-serif",
+    "alignment": "right"
+}, runTests, true);
 
 function randomInt(start, end) {
   return Math.floor(start + (end-start)*Math.random());
@@ -73,14 +95,12 @@ function generateTestConfig() {
   }
 }
 
-runTests();
-
 function runTests() {
   var testConfig = tests.shift() || generateTestConfig();
   run(testConfig, runTests);
 }
 
-function run(test, done) {
+function run(test, done, skipValidation) {
   var textNode = document.createTextNode(test.text);
   var container = document.createElement('div');
   var padding = 5;
@@ -100,72 +120,102 @@ function run(test, done) {
   //  test centering after container has been fit
   //  fillText uses requestAnimationFrame for first fit, so will
   //  add the check to an animation frame after first fit
-  requestAnimationFrame(function () {
-    var inner = container.firstChild.firstChild.getBoundingClientRect();
-    var outer = container.firstChild.getBoundingClientRect();
-    var wrapper = container.getBoundingClientRect();
+  if (!skipValidation) {
+    requestAnimationFrame(function () {
+      var inner = container.firstChild.firstChild.getBoundingClientRect();
+      var outer = container.firstChild.getBoundingClientRect();
+      var wrapper = container.getBoundingClientRect();
 
-    var innerLeft = Math.round(inner.left-outer.left);
-    var innerRight = Math.round(outer.right-inner.right);
-    var innerTop = Math.round(inner.top-outer.top);
-    var innerBottom = Math.round(outer.bottom-inner.bottom);
+      var innerLeft = Math.round(inner.left-outer.left);
+      var innerRight = Math.round(outer.right-inner.right);
+      var innerTop = Math.round(inner.top-outer.top);
+      var innerBottom = Math.round(outer.bottom-inner.bottom);
 
-    //  ensure inner is inside of outer
-    var innerContained = true;
-    if (innerLeft < -0.1) {
-      innerContained = false;
-    }
-    if (innerRight < -0.1) {
-      innerContained = false;
-    }
-    if (innerTop < -0.1) {
-      innerContained = false;
-    }
-    if (innerBottom < -0.1) {
-      innerContained = false;
-    }
+      //  ensure inner is inside of outer
+      var innerContained = true;
+      if (innerLeft < -0.1) {
+        console.log('inner too far left');
+        innerContained = false;
+      }
+      if (innerRight < -0.1) {
+        console.log('inner too far right');
+        innerContained = false;
+      }
+      if (innerTop < -0.1) {
+        console.log('inner too high');
+        innerContained = false;
+      }
+      if (innerBottom < -0.1) {
+        console.log('inner too low');
+        innerContained = false;
+      }
 
-    //  ensure alignment is as expected
-    var aligned = true;
-    if (/top/.test(test.alignment) && Math.round(innerTop) !=0) {
-      aligned = false;
-    }
-    if (/left/.test(test.alignment) && innerLeft != 0) {
-      aligned = false;
-    }
-    if (/right/.test(test.alignment) && innerRight != 0) {
-      aligned = false;
-    }
-    if (/bottom/.test(test.alignment) && innerBottom != 0) {
-      aligned = false;
-    }
-    if (test.alignment == '' && (innerTop != innerBottom || innerLeft != innerRight)) {
-      aligned = false;
-    }
+      //  ensure alignment is as expected
+      var aligned = true;
+      if (/top/.test(test.alignment) && Math.round(innerTop) !=0) {
+        console.log('inner not aligned top');
+        aligned = false;
+      }
+      if (/left/.test(test.alignment) && innerLeft != 0) {
+        console.log('inner not aligned left');
+        aligned = false;
+      }
+      if (/right/.test(test.alignment) && innerRight != 0) {
+        console.log('inner not aligned right');
+        aligned = false;
+      }
+      if (/bottom/.test(test.alignment) && innerBottom != 0) {
+        console.log('inner not aligned bottom');
+        aligned = false;
+      }
+      if (test.alignment == '' && (innerTop != innerBottom || innerLeft != innerRight)) {
+        console.log('inner not aligned center');
+        aligned = false;
+      }
 
-    //  calculate extra space left
-    var extraSpace = Math.min(outer.width - inner.width, outer.height - inner.height);
+      //  calculate extra space left
+      var extraSpace = Math.min(outer.width - inner.width, outer.height - inner.height);
 
-    // ensure spacer inside of container is respecting padding
-    var outerLeft = Math.round(outer.left-wrapper.left);
-    var outerRight = Math.round(wrapper.right-outer.right);
-    var outerTop = Math.round(outer.top-wrapper.top);
-    var outerBottom = Math.round(wrapper.bottom-outer.bottom);
-    var paddingRespected = outerLeft == padding && outerRight == padding && outerTop == 5 && outerBottom == 5;
+      // ensure spacer inside of container is respecting padding
+      var outerLeft = Math.round(outer.left-wrapper.left);
+      var outerRight = Math.round(wrapper.right-outer.right);
+      var outerTop = Math.round(outer.top-wrapper.top);
+      var outerBottom = Math.round(wrapper.bottom-outer.bottom);
+      var paddingRespected = true;
+      if (!(outerLeft == padding && outerRight == padding && outerTop == 5 && outerBottom == 5)) {
+        console.log('padding not respected');
+        paddingRespected = false;
+      }
 
 
-    //  ensure inner is within tolerance
-    if (aligned && innerContained && extraSpace >= 0 && extraSpace < outer.width*0.1 && paddingRespected) {
-      document.body.removeChild(container);
-      done();
-    }
-    else {
-      console.log('failed test');
-      console.log(JSON.stringify(test, null, 4), 'not aligned/contained', inner, outer,
-        outerLeft, outerRight, outerTop, outerBottom);
-      done();
-    }
-  });
+      var withinExtraSpaceTolerance = true;
+      if (extraSpace > outer.width*0.1) {
+        console.log('extra space too big')
+        withinExtraSpaceTolerance = false;
+      }
+
+      //  ensure inner is within tolerance
+      if (aligned && innerContained && extraSpace >= 0 && withinExtraSpaceTolerance && paddingRespected) {
+        passed += 1;
+        $('.passed ').text(passed + '/' + (passed + failed) + ' ' + parseFloat(Math.round(passed/(passed + failed)*1000)/10)+'%');
+        document.body.removeChild(container);
+        done();
+      }
+      else {
+        failed += 1;
+        $('.passed ').text(passed + '/' + (passed + failed));
+        console.log(JSON.stringify(test, null, 4), 'not aligned/contained', inner, outer,
+          outerLeft, outerRight, outerTop, outerBottom);
+        done();
+      }
+    });
+  }
+  else {
+    passed += 1;
+    $('.passed ').text(passed + '/' + (passed + failed));
+    document.body.removeChild(container);
+    done();
+  }
 }
 
 function style(element, styles) {
