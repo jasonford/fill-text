1ab6538dda4e6ccadaed04fa45edb01fff18f901
Jason Ford <fordjason@gmail.com>
2020-02-01 19:18:46 -0800

    Successfully start pulling out jQuery

--- index.js
+++ index.js
@@ -1,3 +1,102 @@
+var cssExpand = [ "Top", "Right", "Bottom", "Left" ];
+
+function augmentWidthOrHeight( elem, name, extra, isBorderBox, styles ) {
+    var i = extra === ( isBorderBox ? "border" : "content" ) ?
+
+        // If we already have the right measurement, avoid augmentation
+        4 :
+
+        // Otherwise initialize for horizontal or vertical properties
+        name === "width" ? 1 : 0,
+
+        val = 0;
+
+    for ( ; i < 4; i += 2 ) {
+
+        // Both box models exclude margin, so add it if we want it
+        if ( extra === "margin" ) {
+            val += jQuery.css( elem, extra + cssExpand[ i ], true, styles );
+        }
+
+        if ( isBorderBox ) {
+
+            // border-box includes padding, so remove it if we want content
+            if ( extra === "content" ) {
+                val -= jQuery.css( elem, "padding" + cssExpand[ i ], true, styles );
+            }
+
+            // At this point, extra isn't border nor margin, so remove border
+            if ( extra !== "margin" ) {
+                val -= jQuery.css( elem, "border" + cssExpand[ i ] + "Width", true, styles );
+            }
+        } else {
+
+            // At this point, extra isn't content, so add padding
+            val += jQuery.css( elem, "padding" + cssExpand[ i ], true, styles );
+
+            // At this point, extra isn't content nor padding, so add border
+            if ( extra !== "padding" ) {
+                val += jQuery.css( elem, "border" + cssExpand[ i ] + "Width", true, styles );
+            }
+        }
+    }
+
+    return val;
+}
+
+
+function getWidthOrHeight( elem, name, extra ) {
+
+    // Start with offset property, which is equivalent to the border-box value
+    var val,
+        valueIsBorderBox = true,
+        styles = window.getComputedStyle( elem ),
+        isBorderBox = jQuery.css( elem, "boxSizing", false, styles ) === "border-box";
+
+    // Support: IE <=11 only
+    // Running getBoundingClientRect on a disconnected node
+    // in IE throws an error.
+    if ( elem.getClientRects().length ) {
+        val = elem.getBoundingClientRect()[ name ];
+    }
+
+    // Some non-html elements return undefined for offsetWidth, so check for null/undefined
+    // svg - https://bugzilla.mozilla.org/show_bug.cgi?id=649285
+    // MathML - https://bugzilla.mozilla.org/show_bug.cgi?id=491668
+    if ( val <= 0 || val == null ) {
+
+        // Fall back to computed then uncomputed css if necessary
+        val = curCSS( elem, name, styles );
+        if ( val < 0 || val == null ) {
+            val = elem.style[ name ];
+        }
+
+        // Computed unit is not pixels. Stop here and return.
+        if ( rnumnonpx.test( val ) ) {
+            return val;
+        }
+
+        // Check for style in case a browser which returns unreliable values
+        // for getComputedStyle silently falls back to the reliable elem.style
+        valueIsBorderBox = isBorderBox &&
+            ( support.boxSizingReliable() || val === elem.style[ name ] );
+
+        // Normalize "", auto, and prepare for extra
+        val = parseFloat( val ) || 0;
+    }
+
+    // Use the active box-sizing model to add/subtract irrelevant styles
+    return ( val +
+        augmentWidthOrHeight(
+            elem,
+            name,
+            extra || ( isBorderBox ? "border" : "content" ),
+            valueIsBorderBox,
+            styles
+        )
+    );
+}
+
 function fillText(element, alignment) {
     function style(el, styles) {
         for (var field in styles) {
@@ -5,16 +104,20 @@ function fillText(element, alignment) {
         }
     }
 
-    function width(el, width) {
-        return width? $(el).width(width) : $(el).width();
-        if (width !== undefined) { el.style.width = width + 'px'; }
-        return parseFloat(window.getComputedStyle(el, null).getPropertyValue('width'));
+    function width(el) {
+        return getWidthOrHeight(el, 'width', 'content');
     }
 
-    function height(el, height) {
-        return height? $(el).height(height) : $(el).height();
-        if (height !== undefined) { el.style.height = height + 'px';}
-        return parseFloat(window.getComputedStyle(el, null).getPropertyValue('height'));
+    function height(el) {
+        return getWidthOrHeight(el, 'height', 'content');
+    }
+
+    function setWidth(el, width) {
+        el.style.width = width;
+    }
+
+    function setHeight(el, height) {
+        el.style.height = height;
     }
 
     var children = element.childNodes;
@@ -48,12 +151,12 @@ function fillText(element, alignment) {
         var text_aspect_ratio = width(text_element) / height(text_element);
         var aspect_ratio = width(element) / height(element);
         if (text_aspect_ratio > aspect_ratio) {
-            width(sizer, width(text_element));
-            height(sizer, width(text_element) / aspect_ratio);
+            setWidth(sizer, width(text_element));
+            setHeight(sizer, width(text_element) / aspect_ratio);
         }
         else {
-            height(sizer, height(text_element));
-            width(sizer, height(text_element) * aspect_ratio);
+            setHeight(sizer, height(text_element));
+            setWidth(sizer, height(text_element) * aspect_ratio);
         }
         var w = width(sizer);
         var h = height(sizer);
@@ -61,11 +164,11 @@ function fillText(element, alignment) {
         while (true) {
             w *= scaleFactor;
             h *= scaleFactor;
-            width(sizer, w);
-            height(sizer, h);
+            setWidth(sizer, w);
+            setHeight(sizer, h);
             if (width(sizer) < width(text_element)|| height(sizer) < height(text_element)) {
-                width(sizer, w/scaleFactor);
-                height(sizer, h/scaleFactor);
+                setWidth(sizer, w/scaleFactor);
+                setHeight(sizer, h/scaleFactor);
                 break;
             }
         }
